791bb4f238218a1dcbfb60b335b007d4bedaa539
Matt Dancho <mdancho@gmail.com>
2016-05-02 18:25:01 -0400

    minor: add image border & shadow

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ $( document ).ready(function() {
 	// Centers blog images
 	$('#blog p > img').addClass('img-responsive center-block');
 
+	$('#blog_content').find('img').addClass('shadow');
 });
 
 
